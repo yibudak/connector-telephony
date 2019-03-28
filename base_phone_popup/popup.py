ee3a0ec542248f9bbbc5637340d89c150e923946
onurugur <onurugur@gitlab.com>
2019-03-28 11:03:24 +0300

    openerp to odoo

--- base_phone_popup/popup.py
+++ base_phone_popup/popup.py
@@ -2,7 +2,7 @@
 # © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api, _
+from odoo import models, fields, api, _
 import logging
 
 
